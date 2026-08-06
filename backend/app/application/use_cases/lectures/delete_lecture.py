db7bfa5b21d1890005a9f3954142210a702ec0e5
Ганжа Андрей Игоревич <aganzha@aedon.ru>
2026-08-06 14:30:21 +0300

    fix: complete section and lecture deletion use cases

--- backend/app/application/use_cases/lectures/delete_lecture.py
+++ backend/app/application/use_cases/lectures/delete_lecture.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from uuid import UUID
 
 from app.application.interfaces.unit_of_work import UnitOfWork
+from app.application.exceptions import LectureNotFoundError
 
 
 @dataclass(slots=True)
@@ -14,4 +15,11 @@ class DeleteLectureUseCase:
         self.uow = uow
 
     async def execute(self, command: DeleteLectureCommand) -> None:
-        ...
+        async with self.uow:
+            lection = await self.uow.lectures.get_by_id(command.lecture_id)
+
+            if lection is None:
+                raise LectureNotFoundError("Lecture not found")
+
+            await self.uow.lectures.delete(command.lecture_id)
+            await self.uow.commit()
--- backend/app/application/use_cases/sections/delete_section.py
+++ backend/app/application/use_cases/sections/delete_section.py
@@ -2,16 +2,24 @@ from dataclasses import dataclass
 from uuid import UUID
 
 from app.application.interfaces.unit_of_work import UnitOfWork
+from app.application.exceptions import SectionNotFoundError
 
 
 @dataclass(slots=True)
 class DeleteSectionCommand:
-    lecture_id: UUID
+    section_id: UUID
 
 
 class DeleteSectionUseCase:
     def __init__(self, uow: UnitOfWork) -> None:
         self.uow = uow
 
-    async def execute(self, command: DeleteLectureCommand) -> None:
-        ...
+    async def execute(self, command: DeleteSectionCommand) -> None:
+        async with self.uow:
+            section = await self.uow.sections.get_by_id(command.section_id)
+
+            if section is None:
+                raise SectionNotFoundError("Section not found")
+
+            await self.uow.sections.delete(command.section_id)
+            await self.uow.commit()
